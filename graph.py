23a17bbceff9703b84ce01cfdfc1243a22c4569b
sdfing <sdfinging@gmail.com>
2024-04-09 10:54:49 +0800

    did sth

--- graph.py
+++ graph.py
@@ -1,16 +1,18 @@
-import student
 import pandas as pd
+import pysnowflake
+from database import db
+from models import KnowledgePointEdge, KnowledgePoint
 
 
 # 定义一个有向图类
 class DiGraph:
     # 初始化一个空的有向图
-    def __init__(self,lesson_name):
+    def __init__(self, lesson_name):
         self.nodes = set()  # 存储所有的点
         self.edges = dict()  # 存储每个点的出边，用一个字典表示，键是点，值是一个集合，存储该点的所有邻居
         self.in_edges = dict()  # 存储每个点的入边，用一个字典表示，键是点，值是一个集合，存储所有指向该点的点
         self.name = dict()  # 存储每个点的中文名字，用一个字典表示，键是点，值是一个字符串
-        self.current_id = 0  # 存储当前的id值，用一个整数表示，初始值为0
+        # self.current_id = 0  # 存储当前的id值，用一个整数表示，初始值为0
         self.lessonName = lesson_name  # 表明这个有向图从属于哪个课程
 
     # 添加一个点
@@ -26,7 +28,6 @@ class DiGraph:
     def add_edge(self, u, v):
         self.edges[u].add(v)  # 把v加入到u的出边集合中
         self.in_edges[v].add(u)  # 把u加入到v的入边集合中
-
 
     # 返回所有的点
     def get_nodes(self):
@@ -68,23 +69,52 @@ class DiGraph:
             # 如果不在图中，就返回一个提示信息
             return "No such name in the graph."
 
+    def save_to_database(self):
+        with db.session.no_autoflush:
+            for node in self.nodes:
+                name = self.name[node]
+                course = self.lessonName
+                knowledge_point = KnowledgePoint.query.filter_by(KnowledgeName=name, KnowledgeBelong=course).first()
+                if not knowledge_point:
+                    knowledge_point = KnowledgePoint(KnowledgeName=name, KnowledgeBelong=course)
+                    db.session.add(knowledge_point)
+            db.session.flush()
+
+            for source, target in self.get_edges():
+                source_name = self.name[source]
+                target_name = self.name[target]
+                source_course = self.lessonName
+                target_course = self.lessonName
+                source_kp = KnowledgePoint.query.filter_by(KnowledgeName=source_name,
+                                                           KnowledgeBelong=source_course).first()
+                target_kp = KnowledgePoint.query.filter_by(KnowledgeName=target_name,
+                                                           KnowledgeBelong=target_course).first()
+                if source_kp and target_kp:
+                    edge = KnowledgePointEdge(sourceID=source_kp.KnowledgeID, targetID=target_kp.KnowledgeID,
+                                              KnowledgeBelong=self.lessonName)
+                    db.session.add(edge)
+
+        db.session.commit()
 
     # 利用pandas读取excel表格
     def read_from_excel(self, file_name):
         # 读取Excel文件
         df = pd.read_excel(file_name)
 
+        # 初始化雪花ID生成器
+        snowflake_generator = pysnowflake.SnowflakeGenerator()
+
         # 检查知识点列是否存在
         if '一级知识点' in df.columns:
             KlgPtL1 = df['一级知识点'].tolist()  # 获取一级知识点的列表
-            # 创建一个字典，用来存储每个中文名字对应的id
+            # 创建一个字典,用来存储每个中文名字对应的id
             name_to_id = dict()
             # 遍历每个一级知识点
             for k in KlgPtL1:
-                # 添加一个新的顶点，并给它赋值name属性
-                self.add_node(k)
-                # 获取该顶点的id
-                id = self.current_id
+                # 生成一个雪花ID
+                id = snowflake_generator.get_guid()
+                # 添加一个新的顶点,并给它赋值name属性
+                self.add_node(k, id)
                 # 把该顶点的中文名字和id存入字典中
                 name_to_id[k] = id
                 # 获取该知识点的前驱知识点的列名
@@ -97,15 +127,15 @@ class DiGraph:
                         pre_k = df.loc[df['一级知识点'] == k, col].values[0]
                         # 如果前驱知识点不为空
                         if not pd.isnull(pre_k) and pre_k != " ":
-                            # 如果前驱知识点不在字典中，就添加它
+                            # 如果前驱知识点不在字典中,就添加它
                             if pre_k not in name_to_id:
-                                self.add_node(pre_k)
-                                # 获取前驱知识点的id
-                                pre_id = self.current_id
+                                # 生成一个雪花ID
+                                pre_id = snowflake_generator.get_guid()
+                                self.add_node(pre_k, pre_id)
                                 # 把前驱知识点的中文名字和id存入字典中
                                 name_to_id[pre_k] = pre_id
                             else:
-                                # 如果前驱知识点已经在字典中，就直接获取它的id
+                                # 如果前驱知识点已经在字典中,就直接获取它的id
                                 pre_id = name_to_id[pre_k]
                             # 添加一条从前驱知识点到一级知识点的边
                             self.add_edge(pre_id, id)
@@ -119,15 +149,15 @@ class DiGraph:
                         sub_k = df.loc[df['一级知识点'] == k, col].values[0]
                         # 如果二级知识点不为空
                         if not pd.isnull(sub_k) and sub_k != " ":
-                            # 如果二级知识点不在字典中，就添加它
+                            # 如果二级知识点不在字典中,就添加它
                             if sub_k not in name_to_id:
-                                self.add_node(sub_k)
-                                # 获取二级知识点的id
-                                sub_id = self.current_id
+                                # 生成一个雪花ID
+                                sub_id = snowflake_generator.get_guid()
+                                self.add_node(sub_k, sub_id)
                                 # 把二级知识点的中文名字和id存入字典中
                                 name_to_id[sub_k] = sub_id
                             else:
-                                # 如果二级知识点已经在字典中，就直接获取它的id
+                                # 如果二级知识点已经在字典中,就直接获取它的id
                                 sub_id = name_to_id[sub_k]
                             # 添加一条从二级知识点到一级知识点的边
                             self.add_edge(sub_id, id)
@@ -135,11 +165,7 @@ class DiGraph:
             print("Error: One or more required columns are missing.")
 
 
-
 # 创建一个有向图对象
 g = DiGraph("数据结构")
 # 实际数据是一个excel，所以还是用函数喵
 g.read_from_excel("data/KlgPts.xlsx")
-
-
-
---
+++ models.py
@@ -0,0 +1,180 @@
+from werkzeug.security import generate_password_hash
+from database import db
+from sqlalchemy import text
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+
+
+class Course(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True, nullable=False)
+    description = db.Column(db.Text)
+    teacher = db.Column(db.String(32))
+
+    def __repr__(self):
+        return '<Course %r>' % self.name
+
+
+class Student(db.Model):
+    __tablename__ = 'student'  # 学生表名
+    student_id = db.Column(db.String(255), primary_key=True, name='学号')
+    name = db.Column(db.String(255), name='姓名')
+    password_hash = db.Column(db.String(255), name='登录密码')
+    major = db.Column(db.String(255), name='专业')
+    class_id = db.Column(db.String(255), name='班级')
+    # knowledge_scores = relationship('StudentKnowledge', back_populates='student')
+
+
+    # ... 其他个人信息字段 ...
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+
+def get_or_create_student(student_id, name, major, class_id, password):
+
+
+    """
+    从选课表中查询学生的信息，如果学生不存在则创建新记录。
+
+    :param student_id: 学生的学号。
+    :param name: 学生的姓名。
+    :param major: 学生的专业。
+    :param class_id: 学生的班级。
+    :param password: 学生的登录密码。
+    :return: Student对象。
+    """
+    student = Student.query.filter_by(student_id=student_id).first()
+    if not student:
+        student = Student(
+            student_id=student_id,
+            name=name,
+            major=major,
+            class_id=class_id,
+            password_hash='1'
+        )
+        student.set_password(password)
+        db.session.add(student)
+        db.session.commit()
+    return student
+
+
+def extract_and_save_student_info():
+    """
+    从选课表中提取所有学生的信息，并保存到学生信息表中。
+    """
+    # 查询选课表中的所有不同学生的信息
+    sql_query = text("""
+        SELECT DISTINCT
+            学号,
+            姓名,
+            专业,
+            班级
+        FROM
+            enrollment
+    """)
+    results = db.session.execute(sql_query)
+
+    # 遍历查询结果，为每个学生创建或更新记录
+    for row in results:
+        student_id, name, major, class_id = row
+        # 检查学生是否已存在
+        existing_student = Student.query.filter_by(student_id=student_id).first()
+        if not existing_student:
+            # 学生不存在，创建新记录
+            new_student = Student(
+                student_id=student_id,
+                name=name,
+                major=major,
+                class_id=class_id
+            )
+            # 设置一个默认密码，实际应用中应该让用户自己设置或生成随机密码
+            default_password = 'default_password'
+            # 设置默认密码或生成随机密码
+            new_student.set_password(default_password)
+            db.session.add(new_student)
+
+    # 提交所有新记录到数据库
+    db.session.commit()
+
+
+class KnowledgePoint(db.Model):
+    __tablename__ = 'knowledge_point'
+    KnowledgeID = db.Column(db.Integer, name='知识点id', primary_key=True)
+    KnowledgeName = db.Column(db.String(255), name='知识点名称', unique=True)
+    KnowledgeBelong = db.Column(db.String(255), name='所属课程')
+
+
+class KnowledgePointEdge(db.Model):
+    __tablename__ = 'knowledge_point_edge'
+    sourceID = db.Column(db.Integer, name='source', primary_key=True)
+    targetID = db.Column(db.Integer, name='target', primary_key=True)
+    KnowledgeBelong = db.Column(db.String(255), name='所属课程')
+    # 其他字段...
+
+class Enrollment(db.Model):
+    __tablename__ = 'enrollment'  # 表名选课表
+    class_id = db.Column(db.String(255), name='班级')
+    student_id = db.Column(db.String(255), primary_key=True, name='学号')
+    name = db.Column(db.String(255), name='姓名')
+    course_name = db.Column(db.String(255), name='课程名称')
+    grade = db.Column(db.String(255), name='成绩')
+    academic_year = db.Column(db.String(255), name='学年')
+    semester = db.Column(db.String(255), name='学期')
+    student_category = db.Column(db.String(255), name='学生类别')
+    college = db.Column(db.String(255), name='学院')
+    major = db.Column(db.String(255), name='专业')
+    major_direction = db.Column(db.String(255), name='专业方向', nullable=True)
+    grade_year = db.Column(db.String(255), name='年级', nullable=True)
+    student_tag = db.Column(db.String(255), name='学生标记', nullable=True)
+    course_college = db.Column(db.String(255), name='开课学院')
+    course_code = db.Column(db.String(255), name='课程代码')
+    teaching_class = db.Column(db.String(255), name='教学班')
+    instructor = db.Column(db.String(255), name='任课教师')
+    credit = db.Column(db.Float, name='学分')
+    grade_remark = db.Column(db.String(255), name='成绩备注', nullable=True)
+    exam_nature = db.Column(db.String(255), name='考试性质')
+    gpa = db.Column(db.Float, name='绩点')
+    course_tag = db.Column(db.String(255), name='课程标记', nullable=True)
+    course_category = db.Column(db.String(255), name='课程类别')
+    course_belonging = db.Column(db.String(255), name='课程归属')
+    course_nature = db.Column(db.String(255), name='课程性质')
+    assessment_method = db.Column(db.String(255), name='考核方式')
+    is_grade_invalid = db.Column(db.String(255), name='是否成绩作废')
+    submitter = db.Column(db.String(255), name='提交人')
+    submission_time = db.Column(db.DateTime, name='提交时间')
+    is_degree_course = db.Column(db.String(255), name='是否学位课程')
+    single_class_retake = db.Column(db.String(255), name='单开班重修', nullable=True)
+    gender = db.Column(db.String(255), name='性别')
+    remark_info = db.Column(db.String(255), name='备注信息', nullable=True)
+    credit_point = db.Column(db.Float, name='学分绩点', nullable=True)
+    course_type = db.Column(db.String(255), name='开课类型')
+
+    def rank_avg_gpa(self, major, grade):
+        from sqlalchemy import text
+        sql_query = text("""
+               SELECT 
+                   学号, 
+                   姓名, 
+                   专业, 
+                   SUM(学分绩点) / SUM(学分) AS 平均学分绩点
+               FROM 
+                   enrollment
+               WHERE 
+                   年级 = :grade AND
+                   专业 = :major
+               GROUP BY 
+                   学号, 姓名, 专业
+               ORDER BY 
+                   平均学分绩点 DESC
+           """)
+        db_results = db.session.execute(sql_query, {'major': major, 'grade': grade})
+        results = list(db_results)
+
+        for i in range(len(results)):
+            results[i] = list(results[i])
+            results[i].append(i + 1)
+        return results
+
+
+
